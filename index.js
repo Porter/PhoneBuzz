be6b08c4fbb487ac8815931aaeb60dfc1690d8a7
Porter <pmh192@gmail.com>
2016-12-02 11:22:00 -0800

    set up twilio library

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const twilio = require("twilio");
 const app = express();
 
+const twilioAccountSID = "AC9730bade7ee8a91b7a7ce491f3061a8d";
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+
 app.get("/", (req, res) => {
-  res.end("And endpoint");
+  res.end(twilioAuthToken + ", " + twilioAccountSID);
 });
 
 const PORT = process.env.PORT || 8000;
